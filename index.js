a9dcd7fc13313859dea108bf21683e6f351166cc
Neesar Banna <Bannasqe@gmail.com>
2024-01-30 16:27:32 -0500

    scope funcitons

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution in this file!
+var customerName = 'bob';
+var bestCustomer;
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+  }
+  
+  function setBestCustomer() {
+    bestCustomer = 'not bob'; 
+    return bestCustomer;
+  }
+  
+  function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+  }
+  
+  setBestCustomer(); // Call setBestCustomer() before overwriteBestCustomer()
+  overwriteBestCustomer();
+  
+  console.log(bestCustomer);
+
+  
+const leastFavoriteCustomer = 'value 1';
+    function changeLeastFavoriteCustomer() {
+        leastFavoriteCustomer = 'overwrite value'; 
+  }
+  changeLeastFavoriteCustomer();
+  console.log(leastFavoriteCustomer);
+
+
